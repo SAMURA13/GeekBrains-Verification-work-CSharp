82adf38f93204162ba7409f0e08932ae02aa83b2
Kos10ko <kos10ko.r@yandex.kz>
2022-12-29 12:06:29 +0600

    add description v2

--- Verification_Home_Work/Program.cs
+++ Verification_Home_Work/Program.cs
@@ -11,13 +11,12 @@ void PrintArray(string[] array)
 }
 void FindResult(string[] array1, string[] array2)
 {
-int count = 0;
+
     for(int i = 0;i < array1.Length;i++)
     {
         if(array1[i].Length <= 3)
         {
-            array2[count] = array1[i];
-            count++;
+            array2[i] = array1[i]; 
         }
     }
 }
