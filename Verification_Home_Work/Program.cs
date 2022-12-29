4bb765968240e3cfd416dfd4cff1fa4baef29431
Kos10ko <kos10ko.r@yandex.kz>
2022-12-29 11:43:17 +0600

    create method findarray

--- Verification_Home_Work/Program.cs
+++ Verification_Home_Work/Program.cs
@@ -1,13 +1,26 @@
-﻿string[] array1 = new string[6] { "1234", "000", "hello", "-2", "result", "GB" };
+﻿string[] array1 = new string[6] { "1234", "lol", "hello", "-2", "result", "GB" };
 string[] array2 = new string[array1.Length];
 
 void PrintArray(string[] array)
-    {
+{
     for(int i = 0;i < array.Length;i++)
         {
         Console.Write($"{array[i]} ");
         }
     Console.WriteLine();
+}
+void FindResult(string[] array1, string[] array2)
+{
+int count = 0;
+    for(int i = 0;i < array1.Length;i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
     }
+}
 
-PrintArray(array1);
+FindResult(array1,array2);
+PrintArray(array2);
