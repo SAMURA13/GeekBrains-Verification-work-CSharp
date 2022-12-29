cf6701a59fe5dc67bea1b115d71bd2603e5853ad
Kos10ko <kos10ko.r@yandex.kz>
2022-12-29 11:40:38 +0600

    create method PrintArray

--- Verification_Home_Work/Program.cs
+++ Verification_Home_Work/Program.cs
@@ -1,2 +1,13 @@
 ﻿string[] array1 = new string[6] { "1234", "000", "hello", "-2", "result", "GB" };
 string[] array2 = new string[array1.Length];
+
+void PrintArray(string[] array)
+    {
+    for(int i = 0;i < array.Length;i++)
+        {
+        Console.Write($"{array[i]} ");
+        }
+    Console.WriteLine();
+    }
+
+PrintArray(array1);
